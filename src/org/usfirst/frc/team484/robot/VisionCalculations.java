c5e7220e9c48fa5d17cb2324d054ab403ebb5cc7
Kevin Connell <keco185@me.com>
2016-02-18 18:51:54 -0500

    Vision touchups

--- src/org/usfirst/frc/team484/robot/VisionCalculations.java
+++ src/org/usfirst/frc/team484/robot/VisionCalculations.java
@@ -4,21 +4,25 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class VisionCalculations {
 	double radiansPerPixel = 0.00133;
+	double radiansPerPixelHorizontal = 0.0012544553999;
 	double cameraHeight = 12.25;
 	double cameraHorizontalOffset = 11.59375;
 	double cameraImageWidth = 640.0;
-	double cameraImageHeight = 479.0;
+	double cameraImageHeight = 480.0;
 	double cameraAngleUp = 0.345915; //0.0174533 converts to radians
 	double heightOfShotInGoal = 15.0;
 	double heightOfGoalBottom = 83.5;
 	double heightOfGoalTop = 95.5;
+	double heightOfGoalCenter = 89.25;
+	double goalWidth = 24;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new VisionCalculations().run();
 	}
 	public void run() {
 		NetworkTable visionTable = NetworkTable.getTable("GRIP/vision");
-			try {
+		try {
 			double[] centerX = visionTable.getNumberArray("centerX", new double[0]);
 			double[] centerY = visionTable.getNumberArray("centerY", new double[0]);
 			double[] area = visionTable.getNumberArray("area", new double[0]);
@@ -35,24 +39,27 @@ public class VisionCalculations {
 				i++;
 			}
 			if (maxAreaSpot > -1) {
-				double verticalAngleBottom = cameraAngleUp + ((cameraImageHeight - centerY[maxAreaSpot]) - height[maxAreaSpot]/2.0) * radiansPerPixel;
-				double verticalAngleTop = cameraAngleUp + ((cameraImageHeight - centerY[maxAreaSpot]) + height[maxAreaSpot]/2.0) * radiansPerPixel;
-				double horizontalAngleCenter = 1.5707963268 - (cameraImageWidth / 2.0 - centerX[maxAreaSpot]) * radiansPerPixel;
+				double horizontalAngleCenter = 1.5707963268 - (cameraImageWidth / 2.0 - centerX[maxAreaSpot]) * radiansPerPixelHorizontal;
+				double horizontalAngleRight = 1.5707963268 - (cameraImageWidth / 2.0 - (centerX[maxAreaSpot] + width[maxAreaSpot])) * radiansPerPixelHorizontal;
+				double verticalAngleCenter =  cameraAngleUp + (cameraImageHeight - centerY[maxAreaSpot]) * radiansPerPixel;
+
+				double distance = (heightOfGoalCenter - cameraHeight)/Math.tan(verticalAngleCenter);
+				double horizontalOffset1 = distance / Math.tan(horizontalAngleCenter) - cameraHorizontalOffset;
+				double horizontalOffset2 = distance / Math.tan(horizontalAngleRight) - (cameraHorizontalOffset - goalWidth/2.0);
 				
-				double distance1 = (heightOfGoalBottom - cameraHeight)/Math.tan(verticalAngleBottom);
-				double distance2 = (heightOfGoalTop - cameraHeight)/Math.tan(verticalAngleTop);
-				double horizontalOffset1 = (Math.cos(horizontalAngleCenter) * distance1 - cameraHorizontalOffset * Math.sin(horizontalAngleCenter))/Math.sin(horizontalAngleCenter);
-				double horizontalOffset2 = (Math.cos(horizontalAngleCenter) * distance2 - cameraHorizontalOffset * Math.sin(horizontalAngleCenter))/Math.sin(horizontalAngleCenter);
-				double shootAngle = Math.atan((heightOfGoalBottom + heightOfShotInGoal)/distance1);
-				System.out.println("D1: " + distance1 + "  h1: " + horizontalOffset1);
-				System.out.println("D2: " + distance2 + "  h2: " + horizontalOffset2);
-				System.out.println("Too low by: " + (shootAngle - (1.5707963268 + Robot.robotIO.shooterArmEncoder.getDistance())));
+				double shootAngle = Math.atan((heightOfGoalCenter + heightOfShotInGoal)/distance);
+				if (centerY[maxAreaSpot] + height[maxAreaSpot] >= 480) {
+					System.out.println("Camera At Max Distance");
+				} else {
+					System.out.println("D: " + distance + "  h1: " + horizontalOffset1 + " h2: " + horizontalOffset2);
+					System.out.println("Too low by: " + (shootAngle - (1.5707963268 + Robot.robotIO.shooterArmEncoder.getDistance())));
+				}
 			} else {
 				System.out.println("Target Not Found");
 			}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
