6ac18e43675a9bc175c84323c44b27151fddd0c1
Kevin <keco185@me.com>
2016-03-14 22:07:03 -0400

    Started Writing Autonomous Outline

--- src/org/usfirst/frc/team484/robot/Robot.java
+++ src/org/usfirst/frc/team484/robot/Robot.java
@@ -3,14 +3,31 @@ package org.usfirst.frc.team484.robot;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
+import org.usfirst.frc.team484.robot.commands.AutoCrossChevalDeFrise;
+import org.usfirst.frc.team484.robot.commands.AutoCrossLowBar;
+import org.usfirst.frc.team484.robot.commands.AutoCrossMoat;
+import org.usfirst.frc.team484.robot.commands.AutoCrossNothing;
+import org.usfirst.frc.team484.robot.commands.AutoCrossPortcullis;
+import org.usfirst.frc.team484.robot.commands.AutoCrossRamparts;
+import org.usfirst.frc.team484.robot.commands.AutoCrossRockWall;
+import org.usfirst.frc.team484.robot.commands.AutoCrossRoughTerrain;
+import org.usfirst.frc.team484.robot.commands.AutoCrossSallyPort;
+import org.usfirst.frc.team484.robot.commands.AutoShootHigh1;
+import org.usfirst.frc.team484.robot.commands.AutoShootHigh2;
+import org.usfirst.frc.team484.robot.commands.AutoShootHigh3;
+import org.usfirst.frc.team484.robot.commands.AutoShootHigh4;
+import org.usfirst.frc.team484.robot.commands.AutoShootHigh5;
+import org.usfirst.frc.team484.robot.commands.AutoShootNothing;
 import org.usfirst.frc.team484.robot.subsystems.Drivetrain;
 import org.usfirst.frc.team484.robot.subsystems.ShooterArm;
 import org.usfirst.frc.team484.robot.subsystems.ShooterPiston;
 import org.usfirst.frc.team484.robot.subsystems.ShooterWheels;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -31,6 +48,11 @@ public class Robot extends IterativeRobot {
 	public static OI oi;
 	CameraServer camera;
 
+	Command autoCrossCommand; //Creates command for crossing the defenses
+    Command autoShootCommand; //Creates a command for shooting after the cross
+    SendableChooser autoCrossChooser; //Creates a choose menu for selecting crossing command on dashboard
+    SendableChooser autoShootChooser; //Creates a choose menu for selecting shooting command on dashboard
+
     public void robotInit() {
 		oi = new OI();
 		robotIO.airCompressor.start();
@@ -40,6 +62,27 @@ public class Robot extends IterativeRobot {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		 autoCrossChooser = new SendableChooser(); //Initializes the choose menu for selecting crossing command
+	        autoCrossChooser.addDefault("Do Nothing", new AutoCrossNothing()); //adds do nothing as an option for crossing
+	        autoCrossChooser.addObject("ChevalDeFrise", new AutoCrossChevalDeFrise()); //adds cheval de frise as an option for crossing
+	        autoCrossChooser.addObject("LowBar", new AutoCrossLowBar()); //adds low bar as an option for crossing
+	        autoCrossChooser.addObject("Moat", new AutoCrossMoat()); //adds moat as an option for crossing
+	        autoCrossChooser.addObject("Portcullis", new AutoCrossPortcullis()); //adds portcullis as an option for crossing
+	        autoCrossChooser.addObject("Ramparts", new AutoCrossRamparts()); //adds ramparts as an option for crossing
+	        autoCrossChooser.addObject("RockWall", new AutoCrossRockWall()); //adds rock wall as an option for crossing
+	        autoCrossChooser.addObject("RoughTerrain", new AutoCrossRoughTerrain()); //adds rough terrain as an option for crossing
+	        autoCrossChooser.addObject("SallyPort", new AutoCrossSallyPort()); //adds sally port as an option for crossing
+	        SmartDashboard.putData("Auto Defence", autoCrossChooser); //Places the choose menu for selecting a crossing command on the dashboard
+	        
+	        autoShootChooser = new SendableChooser(); //Initializes the choose menu for selecting shooting command
+	        autoShootChooser.addDefault("Don't Shoot", new AutoShootNothing()); //adds do nothing as an option for shooting
+	        autoShootChooser.addObject("Pos 1", new AutoShootHigh1()); //adds shoot high from position 1 as an option for shooting
+	        autoShootChooser.addObject("Pos 2", new AutoShootHigh2()); //adds shoot high from position 2 as an option for shooting
+	        autoShootChooser.addObject("Pos 3", new AutoShootHigh3()); //adds shoot high from position 3 as an option for shooting
+	        autoShootChooser.addObject("Pos 4", new AutoShootHigh4()); //adds shoot high from position 4 as an option for shooting
+	        autoShootChooser.addObject("Pos 5", new AutoShootHigh5()); //adds shoot high from position 5 as an option for shooting
+	        SmartDashboard.putData("Auto Position: ", autoShootChooser); //Places the choose menu for selecting a shooting command on the dashboard
+
     }
 	
     public void disabledInit(){
